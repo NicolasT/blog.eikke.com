f09e2221234fcedbac16978c0f709da6a9998575
Nicolas Trangez <ikke@nicolast.be>
2012-12-11 21:29:09 +0100

    Import

---
+++ xmlsrv/rss2319d.php
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="iso-8859-1"?><!-- generator="b2evolution/1.8.6" -->
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:admin="http://webns.net/mvcb/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:content="http://purl.org/rss/1.0/modules/content/">
+	<channel>
+		<title>Peter's Blog</title>
+						<link>http://blog.eikke.com/index.php/peter</link>
+				<description></description>
+				<language>en-EU</language>
+				<docs>http://backend.userland.com/rss</docs>
+				<admin:generatorAgent rdf:resource="http://b2evolution.net/?v=1.8.6"/>
+				<ttl>60</ttl>
+								<item>
+					<title>Moved!</title>
+					<link>http://blog.eikke.com/index.php/peter/2005/08/16/moved</link>
+					<pubDate>Tue, 16 Aug 2005 18:16:42 +0000</pubDate>
+										<category domain="main">Internet &#38; Blogs</category>					<guid isPermaLink="false">403@http://blog.eikke.com/</guid>
+					<description>This blog is moved!
+
+I finally moved my blog to my own domain, peterdedecker.net.  So, if you're reading this through an RSS-feed, please update this entry in your aggregator to the new one.
+
+Of course, this is only applicable to the ones who are just reading Peter's Blog, the blogs of Ikke, RealNitro and the FSU-blog stay at this location.
+
+Thanks for hosting until now, Ikke!</description>
+					<content:encoded><![CDATA[<p>This blog is moved!</p>
+
+<p>I finally moved my blog to my own domain, <a href="http://www.peterdedecker.net/blog/index.php">peterdedecker.net</a>.  So, if you're reading this through an RSS-feed, please update this entry in your aggregator to the new one.</p>
+
+<p>Of course, this is only applicable to the ones who are just reading <a href="http://www.peterdedecker.net">Peter's Blog</a>, the blogs of <a href="http://blog.eikke.com/index.php/ikke">Ikke</a>, <a href="http://blog.eikke.com/index.php/realnitro">RealNitro</a> and the <a href="http://blog.eikke.com/index.php/FSU">FSU-blog</a> stay at this location.</p>
+
+<p>Thanks for hosting until now, Ikke!</p>]]></content:encoded>
+					<comments>http://blog.eikke.com/index.php/peter?p=403&amp;c=1&amp;tb=1&amp;pb=1#comments</comments>
+				</item>
+								<item>
+					<title>Stage gedaan en schatje terug!</title>
+					<link>http://blog.eikke.com/index.php/peter/2005/08/15/stage_gedaan_en_schatje_terug</link>
+					<pubDate>Sun, 14 Aug 2005 23:53:56 +0000</pubDate>
+										<category domain="main">Life &#38; Fun</category>
+<category domain="alt">Siemens</category>					<guid isPermaLink="false">401@http://blog.eikke.com/</guid>
+					<description>Sinds vrijdag is het eindelijk gedaan.  De zes weken stage bij Siemens.  Niet dat 't geen fijne tijd was, daar niet van, maar er mocht wel een einde aan komen.  Elke dag vroeg uit mijn bed moeten en 's avonds laat thuis zijn terwijl alle anderen verlof hebben, is niet zo fijn.  Bovendien wordt 't ook wel s tijd om wat te studeren voor de tweede zit.
+
+Wat de stage betreft: het was zeker de moeite waard.  In een bedrijf als Siemens krijg je eens totaal ander materiaal in je handen.  Tests opstellen voor nieuwe technieken, zelf iets testen, nieuwe technieken bestuderen, meevolgen van en meewerken aan interessante state-off-the-art projecten zoals het internet op de Thalys of I-City,...  En de bende geeks die daar rondliep was ook wel fijn :-)  Ik wist trouwens niet dat Linux zoveel gebruikt werd: settopboxen, access points, routers/gateways,...  Alles draait linux!  U heeft waarschijnlijk meer linux in uw huis dan u dacht ;-)  Echt wel de moeite waard en een aanrader aan al wie twijfelt om een stage te gaan doen!  Dat dit voor zes studiepunten meetelt, is uiteraard uitermate mooi meegenomen.  Maar 't mag ook wel, want zes weken is lang en met de reistijd erbij waren het wel lange dagen.
+
+
+
+Nog meer goed nieuws: mijn schatje is terug!  Jaja, terwijl ik aan't werken was, heeft zij doodleuk een reisje gemaakt naar Normandi</description>
+					<content:encoded><![CDATA[<p>Sinds vrijdag is het eindelijk gedaan.  De zes weken stage bij <a href="http://www.siemens.be">Siemens</a>.  Niet dat 't geen fijne tijd was, daar niet van, maar er mocht wel een einde aan komen.  Elke dag vroeg uit mijn bed moeten en 's avonds laat thuis zijn terwijl alle anderen verlof hebben, is niet zo fijn.  Bovendien wordt 't ook wel s tijd om wat te studeren voor de <a href="http://blog.eikke.com/index.php/peter/2005/07/17/resultaten_warmte_reisjes_joke">tweede zit</a>.</p>
+
+<p>Wat de stage betreft: het was zeker de moeite waard.  In een bedrijf als Siemens krijg je eens totaal ander materiaal in je handen.  Tests opstellen voor nieuwe technieken, zelf iets testen, nieuwe technieken bestuderen, meevolgen van en meewerken aan interessante state-off-the-art projecten zoals het internet op de <a href="http://www.thalys.be">Thalys</a> of <a href="http://www.i-city.be">I-City</a>,...  En de bende geeks die daar rondliep was ook wel fijn :-)  Ik wist trouwens niet dat Linux zoveel gebruikt werd: settopboxen, access points, routers/gateways,...  Alles draait linux!  U heeft waarschijnlijk meer linux in uw huis dan u dacht ;-)  Echt wel de moeite waard en een aanrader aan al wie twijfelt om een stage te gaan doen!  Dat dit voor zes studiepunten meetelt, is uiteraard uitermate mooi meegenomen.  Maar 't mag ook wel, want zes weken is lang en met de reistijd erbij waren het wel lange dagen.</p>
+
+<p><center><a href="http://www.magicalears.com/clipart/Classic%20Characters/Goofy/"><img src="http://www.magicalears.com/clipart/Classic%20Characters/Goofy/goofy-1.gif" alt="Goofy" title="Goofy" /></a></center></p>
+
+<p>Nog meer goed nieuws: mijn schatje is terug!  Jaja, terwijl ik aan't werken was, heeft zij doodleuk een reisje gemaakt naar Normandi</p>]]></content:encoded>
+					<comments>http://blog.eikke.com/index.php/peter?p=401&amp;c=1&amp;tb=1&amp;pb=1#comments</comments>
+				</item>
+								<item>
+					<title>Lezersbrief over affaire Vandamme</title>
+					<link>http://blog.eikke.com/index.php/peter/2005/08/13/lezersbrief_over_affaire_vandamme</link>
+					<pubDate>Sat, 13 Aug 2005 12:16:07 +0000</pubDate>
+										<category domain="main">UGent &#38; Stuver</category>					<guid isPermaLink="false">398@http://blog.eikke.com/</guid>
+					<description>KIM-OPLEIDING
+
+Na het bekendmaken van de ,,Vandamme-affaire'', over het inhoudsloze KIM-postgraduaat aan de Ugent (DS 27 juli) haastten de KULeuven, de VUB en de UA zich om te verklaren dat dergelijke wanpraktijken bij hen niet mogelijk zijn ,,vanwege de vele controles''. Ik betwijfel dat ten zeerste. Over die KIM-opleiding werd wel degelijk een anonieme studentenevaluatie georganiseerd. Geen enkele student, op </description>
+					<content:encoded><![CDATA[<blockquote><p><strong>KIM-OPLEIDING</strong></p>
+
+<p>Na het bekendmaken van de ,,Vandamme-affaire'', over het inhoudsloze KIM-postgraduaat aan de Ugent (DS 27 juli) haastten de KULeuven, de VUB en de UA zich om te verklaren dat dergelijke wanpraktijken bij hen niet mogelijk zijn ,,vanwege de vele controles''. Ik betwijfel dat ten zeerste. Over die KIM-opleiding werd wel degelijk een anonieme studentenevaluatie georganiseerd. Geen enkele student, op </p>]]></content:encoded>
+					<comments>http://blog.eikke.com/index.php/peter?p=398&amp;c=1&amp;tb=1&amp;pb=1#comments</comments>
+				</item>
+								<item>
+					<title>Ook in Frankrijk wordt Open Source in het onderwijs ge</title>
+					<link>http://blog.eikke.com/index.php/peter/2005/08/12/ook_in_frankrijk_wordt_open_source_in_he</link>
+					<pubDate>Fri, 12 Aug 2005 21:30:50 +0000</pubDate>
+										<category domain="main">Free Software</category>					<guid isPermaLink="false">397@http://blog.eikke.com/</guid>
+					<description>Eerder was al te lezen dat het Vlaamse onderwijs de mogelijkheden van vrije software gaat verkennen.  Dit is blijkbaar niet alleen zo in Vlaanderen, maar ook in Frankrijk.  Daar krijgen de middelbare scholieren begin volgend jaar CD's van de overheid met vrije software en een Linux Live CD.  Hopelijk wordt het een groot succes!
+bron</description>
+					<content:encoded><![CDATA[<p>Eerder was al te <a href="http://blog.eikke.com/index.php/peter/2005/06/08/vlaams_onderwijs_verkent_mogelijkheden_v">lezen</a> dat het Vlaamse onderwijs de mogelijkheden van vrije software gaat verkennen.  Dit is blijkbaar niet alleen zo in Vlaanderen, maar ook in Frankrijk.  Daar krijgen de middelbare scholieren begin volgend jaar CD's van de overheid met vrije software en een Linux Live CD.  Hopelijk wordt het een groot succes!<br />
+<a href="http://www.computable.nl/nieuws.htm?id=773817">bron</a></p>]]></content:encoded>
+					<comments>http://blog.eikke.com/index.php/peter?p=397&amp;c=1&amp;tb=1&amp;pb=1#comments</comments>
+				</item>
+								<item>
+					<title>Alles OK met de ruimtevaart</title>
+					<link>http://blog.eikke.com/index.php/peter/2005/08/11/alles_ok_met_de_ruimtevaart</link>
+					<pubDate>Thu, 11 Aug 2005 08:19:43 +0000</pubDate>
+										<category domain="main">Life &#38; Fun</category>					<guid isPermaLink="false">394@http://blog.eikke.com/</guid>
+					<description> Het gaat weer goed met de ruimtevaart.  Na het bange afwachten op het resultaat van de Discovery-missie is iedereen nu terug hoopvol voor de toekomst.  De NASA wist te melden dat de shuttle nog nooit in zo'n goede staat teruggekeerd is van een ruimtereis.  Daarnaast is vandaag ook nog een Ariane 5 vertrokken die meteen het record van de lancering van de zwaarste telecommunicatiesatelliet ooit op haar naam mag schrijven.  Alleen een Ariane 5 is in staat om zo'n beest van 6,5 ton in een geostationaire baan te krijgen.  De Europese en Amerikaanse ruimtevaartorganisaties beleven hoogdagen!</description>
+					<content:encoded><![CDATA[<p><img src="http://www.arianespace.com/site/images/missionupdate_aug9_sm.jpg" alt="Airane 5" align="left" /> Het gaat weer goed met de ruimtevaart.  Na het bange afwachten op het resultaat van de Discovery-missie is iedereen nu terug hoopvol voor de toekomst.  De <a href="http://www.nasa.gov">NASA</a> wist te <a href="http://www.standaard.be/nieuws/buitenland/index.asp?ArticleID=DMF11082005_017&amp;Snel=1">melden</a> dat de shuttle nog nooit in zo'n goede staat teruggekeerd is van een ruimtereis.  Daarnaast is <a href="http://www.standaard.be/nieuws/buitenland/index.asp?ArticleID=DMF11082005_013&amp;Snel=1">vandaag</a> ook nog een <a href="http://www.arianespace.com/site/launcher/future_sub_index.html">Ariane 5</a> vertrokken die meteen het record van de <a href="http://www.arianespace.com/site/news/news_sub_missionupdate_index.html">lancering van de zwaarste telecommunicatiesatelliet</a> ooit op haar naam mag schrijven.  Alleen een Ariane 5 is in staat om zo'n <em>beest</em> van 6,5 ton in een geostationaire baan te krijgen.  De Europese en Amerikaanse ruimtevaartorganisaties beleven hoogdagen!</p>]]></content:encoded>
+					<comments>http://blog.eikke.com/index.php/peter?p=394&amp;c=1&amp;tb=1&amp;pb=1#comments</comments>
+				</item>
+					</channel>
+</rss>
